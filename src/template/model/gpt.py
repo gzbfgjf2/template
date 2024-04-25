cb5a2fe63c0857657aef23ca69405ca5d683710a
gzbfgjf2 <142939562+gzbfgjf2@users.noreply.github.com>
2024-04-25 22:02:38 +0100

    Add generate method to gpt

--- src/template/model/gpt.py
+++ src/template/model/gpt.py
@@ -6,6 +6,9 @@ from ._nn.attention import CausalSelfAttention
 from ._nn.mlp import Mlp
 from ._nn.layer_norm import LayerNorm
 from ._mixin.optimizer import OptimizerMixin
+from pathlib import Path
+import sys
+import time
 
 
 class Block(nn.Module):
@@ -39,6 +42,14 @@ class GPT(OptimizerMixin, nn.Module):
         self.load_weight(config)
 
     def load_weight(self, config):
+        experiment_path = Path(sys.argv[2])
+        checkpoint_path = experiment_path / "checkpoint.ckpt"
+        if checkpoint_path.exists():
+            print("checkpoint exists, load from checkpoint, ignore init_from")
+            checkpoint = torch.load(checkpoint_path, map_location="cpu")
+            self.load_state_dict(checkpoint["model"])
+            return
+
         self.apply(self._init_weights)
         # apply special scaled init to the residual projections, per GPT-2 paper
         for pn, p in self.named_parameters():
@@ -89,6 +100,7 @@ class GPT(OptimizerMixin, nn.Module):
         return logits, loss
 
     def forward(self, idx):
+        a = time.time()
         device = idx.device
         b, t = idx.size()
         pos = torch.arange(0, t, dtype=torch.long, device=device)
@@ -100,3 +112,34 @@ class GPT(OptimizerMixin, nn.Module):
         x = self.ln(x)
         logits = self.lm_head(x)
         return logits
+
+    @torch.no_grad()
+    def generate(self, idx, max_new_tokens=1000, temperature=1.0, top_k=None):
+        """
+        Take a conditioning sequence of indices idx (LongTensor of shape (b,t)) and complete
+        the sequence max_new_tokens times, feeding the predictions back into the model each time.
+        Most likely you'll want to make sure to be in model.eval() mode of operation for this.
+        """
+        for _ in range(max_new_tokens):
+            # if the sequence context is growing too long we must crop it at block_size
+            idx_cond = (
+                idx
+                if idx.size(1) <= self.config.sequence_length
+                else idx[:, -self.config.sequence_length :]
+            )
+            # forward the model to get the logits for the index in the sequence
+            logits = self(idx_cond)
+            # pluck the logits at the final step and scale by desired temperature
+            logits = logits[:, -1, :] / temperature
+            # optionally crop the logits to only the top k options
+            if top_k is not None:
+                v, _ = torch.topk(logits, min(top_k, logits.size(-1)))
+                logits[logits < v[:, [-1]]] = -float("Inf")
+            # apply softmax to convert logits to (normalized) probabilities
+            probs = F.softmax(logits, dim=-1)
+            # sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1)
+            # append sampled index to the running sequence and continue
+            idx = torch.cat((idx, idx_next), dim=1)
+
+        return idx
